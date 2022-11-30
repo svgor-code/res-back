0d6e77f1d7e2e0a1ca02a11d5dd086976bc2c282
cryalonebaby <cryalonebabyyy@gmail.com>
2022-11-30 20:12:51 +0200

    did the test and changed update-order-dto

--- src/dto/update-order.dto.ts
+++ src/dto/update-order.dto.ts
@@ -1,7 +1,6 @@
-import { OrderStatusEnum } from '@prisma/client'
+import { OrderStatusEnum } from '@prisma/client';
 
 export class UpdateOrderDto {
-  id: number;
   status: OrderStatusEnum;
   persons: number;
   createdAt: string | Date;
